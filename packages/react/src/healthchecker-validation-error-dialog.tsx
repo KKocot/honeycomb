c8776662b12de2945130d534cb9ba89e79dd6c9f
jlachor <jlachor@syncad.com>
2026-02-19 10:52:11 +0100

    Fix style pronlems

--- packages/react/src/healthchecker-validation-error-dialog.tsx
+++ packages/react/src/healthchecker-validation-error-dialog.tsx
@@ -36,8 +36,8 @@ export function ValidationErrorDialog({
   return (
     <Dialog.Root open={isOpened} onOpenChange={onDialogOpenChange}>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 z-50 bg-black/80" />
-        <Dialog.Content className="fixed left-1/2 top-1/2 z-50 grid w-full max-w-lg -translate-x-1/2 -translate-y-1/2 gap-4 border bg-gray-800 text-white p-6 shadow-lg sm:rounded-lg">
+        <Dialog.Overlay className="fixed inset-0 z-50 bg-black/80 flex items-center justify-center">
+        <Dialog.Content className="relative grid w-full max-w-lg gap-4 border bg-gray-800 text-white p-6 shadow-lg sm:rounded-lg">
           <Dialog.Close className="absolute right-4 top-4 rounded-sm opacity-70 hover:opacity-100">
             <X className="h-4 w-4" />
             <span className="sr-only">Close</span>
@@ -72,6 +72,7 @@ export function ValidationErrorDialog({
             </button>
           </div>
         </Dialog.Content>
+        </Dialog.Overlay>
       </Dialog.Portal>
     </Dialog.Root>
   );
